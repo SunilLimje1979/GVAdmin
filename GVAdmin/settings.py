5308fe4c934c0d00b84a713d69ba4a6b673a178f
Pawarshekhar709 <pawarshekhar708@gmail.com>
2025-09-17 14:50:59 +0530

    commit message

--- GVAdmin/settings.py
+++ GVAdmin/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = "/static/" #GVAdmin
-# STATIC_URL = "/GVAdmin/static/" #hrms_admin
+# STATIC_URL = "/static/" #GVAdmin
+STATIC_URL = "/GVAdmin/static/" #hrms_admin
 STATICFILES_DIRS = [
-    # BASE_DIR / "GVAdmin/static",
-     BASE_DIR / "static",
+    BASE_DIR / "GVAdmin/static",
+    #  BASE_DIR / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media settings
